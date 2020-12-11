aa0a76b13cbb54abd782a46d24f7e6a7f795f90d
Vulture2009 <73707254+Vulture2009@users.noreply.github.com>
2020-12-12 00:10:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+function setup()
+{
+    canvas = createCanvas(600, 350);
+    canvas.center();
+}
+
+function draw()
+{
+    fill(255, 0, 0);
+    circle(50, 315, 50);
+    circle(50, 230, 50);
+    circle(50, 145, 50);
+    circle(50, 60, 50);
+    circle(130, 60, 50);
+    circle(210, 60, 50);
+    circle(290, 60, 50);
+    circle(370, 60, 50);
+    circle(450, 60, 50);
+    circle(530, 60, 50);
+    circle(530, 145, 50);
+    circle(530, 230, 50);
+    circle(530, 315, 50);
+    circle(450, 315, 50);
+    circle(370, 315, 50);
+    circle(290, 315, 50);
+    circle(210, 315, 50);
+    circle(130, 315, 50);
+}
